dc02f8ec3f40740597002c09910a1ed5cbe67925
danielalfb <danielalfbarbosa@gmail.com>
2020-06-11 10:17:21 -0300

    endpoint 4 and 5

--- routes/grades.js
+++ routes/grades.js
@@ -48,12 +48,14 @@ router.put('/', async (req, res) => {
       (grade) => grade.id === updateGrade.id
     );
     //atualização de campos específicos dentro da api
+    json.grades[oldIndex].student = updateGrade.student;
+    json.grades[oldIndex].subject = updateGrade.subject;
+    json.grades[oldIndex].type = updateGrade.type;
     json.grades[oldIndex].value = updateGrade.value;
 
     await writeFile(jsonURL, JSON.stringify(json));
     res.send(updateGrade);
     res.end();
-
   } catch (err) {
     res.status(400).send({
       error: err.message,
@@ -82,5 +84,64 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+//endpoint para consultar uma grade
+router.get('/:id', async (req, res) => {
+  try {
+    let data = await readFile(jsonURL, 'utf8');
+    let json = JSON.parse(data);
+
+    const gradeFound = json.grades.find(
+      (grade) => grade.id === parseInt(req.params.id, 10)
+    );
+    if (gradeFound) {
+      res.send(gradeFound);
+    } else {
+      res.send('Grade not found');
+      res.end();
+    }
+  } catch (err) {
+    res.status(400).send({
+      error: err.message,
+    });
+  }
+});
+
+//endpoint para consultar a nota total de um aluno em uma disciplina
+router.post('/sum', async (req, res) => {
+  let gradeReq = req.body;
+  try {
+    let data = await readFile(jsonURL, 'utf8');
+    let json = JSON.parse(data);
+
+    let student = json.grades.filter((grade) => grade.student === gradeReq.student);
+    let subject = student.filter((student) => student.subject === gradeReq.subject);
+    let gradeSum = subject.reduce((acc, curr) => acc + curr.value, 0);
+    res.send(
+      `${gradeReq.student}'s total grade for "${gradeReq.subject}" is: ${gradeSum}`
+    );
+  } catch (err) {
+    res.status(400).send({
+      Error: err.message
+    });
+  }
+});
+
+// router.get('/', async (_, res) => {
+//   try {
+//     let data = await readFile(jsonURL, 'utf8');
+//     let json = JSON.parse(data);
+
+//     let grades = json.grades.filter(
+//       (grade) => grade.student === parseInt(req.params.student, 10)
+//     );
+//     let gradesCount = json.grades.value++;
+
+//     res.send(gradesCount);
+//   } catch (err) {
+//     res.status(400).send({
+//       error: err.message,
+//     });
+//   }
+// });
 
 export default router;
